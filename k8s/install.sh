243f9c7651b6b5e2863e9e470d0bf08ef715b579
Csaba Ujvári <csaba.ujvari@commitsmart.com>
2021-07-30 00:42:24 +0200

    fix: etcd auth

--- k8s/install.sh
+++ k8s/install.sh
@@ -7,5 +7,5 @@ kubectl apply -f psql-secret.yaml -n dev
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo add cetic https://cetic.github.io/helm-charts
 
-helm upgrade --install -n dev etcd bitnami/etcd
+helm upgrade --install -n dev --set auth.rbac.enabled="false" etcd bitnami/etcd
 helm upgrade --install -n dev psql cetic/postgresql
